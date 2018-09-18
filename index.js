4d3091bfd8698e64d7752c23d6848df0bf11eb90
Tea&Code <nexus61lu@gmail.com>
2018-09-18 12:46:17 -0700

    Add files via upload
    V1.0.0

---
+++ index.js
@@ -0,0 +1,78 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new SlackBot({
+
+	token:'xoxb-437256997315-437544290789-d7zX8LpjQeV0Np694vsHcufB',
+	name:'WeatherWiz9000'
+
+});
+
+//Start handler
+bot.on('start',()=>{
+	
+ 	bot.postMessageToChannel(
+ 	'general',
+ 	'Hello there, this is weatherWiz9000, Im ready for my first test. Please enter a location to check its weather.'
+
+ );
+})
+
+//Error Handler
+bot.on('error', err => console.log(err));
+
+//Message Handler
+bot.on('message',data => {
+
+	if(data.username !== 'WeatherWiz9000' && data.type == 'message'){
+		messageHandler(data.text);
+		
+	}
+	else 
+		return;
+
+});
+
+function messageHandler(message){
+		
+		if(message.includes('help'))
+			getHelp();
+
+		else if(message.includes('@UCVG08JP7')){
+				const str = message.replace('<@UCVG08JP7> ','')
+				getWeather(str);
+
+		}
+	
+}
+
+function getWeather(message){
+	axios.get('http://api.openweathermap.org/data/2.5/weather?q='+message+'&APPID=3108b6e814e9396d7f54573e06dd1959')
+	.then(function (response) {
+		
+		const des = response.data['weather'][0].description;
+		const temp = response.data['main'].temp;
+		
+			bot.postMessageToChannel(
+ 			'general',
+ 			'Weather at ' + message + " is " + des + " with a temperture of " + temp + "K."
+ 		);
+		
+	})
+	.catch(function(error){
+		
+		bot.postMessageToChannel(
+ 			'general',
+ 			'Im sorry, Im afraid I cant do that.'
+ 		);
+
+	});
+
+}
+
+function getHelp(){
+	bot.postMessageToChannel(
+ 			'general',
+ 			'You can enter by city,zipcode,country or corrdinates.\nExamples: San Mateo, 94402 or USA'
+ 		);
+}
